b5154bbcfd077d7798031fc6df58fb3ee1a0f2dd
Micah Wedemeyer <me@micahwedemeyer.com>
2016-03-24 17:45:05 -0400

    Refactor module structure

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'slim'
 gem 'sass'
+gem 'wrenchmode-rack', path: "../wrenchmode_rack"
--- app.rb
+++ app.rb
@@ -1,10 +1,8 @@
-require 'sinatra'
-require 'slim'
-require 'sass'
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default)
 
-require './redirector'
-
-use Redirector, domain_id: 12, check_delay_secs: 2, logging: true, jwt: "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJEb21haW46MTIiLCJleHAiOjE0NjA2NTY4NzcsImlhdCI6MTQ1ODA2NDg3NywiaXNzIjoiQ3Jhc2hzd2l0Y2giLCJqdGkiOiI3MDIwNTFlOS0wZmNmLTRlZGYtODRlMS0zMWZjMTEzOTg4Y2EiLCJwZW0iOnt9LCJzdWIiOiJEb21haW46MTIiLCJ0eXAiOiJ0b2tlbiJ9.Y90a7cDw_-1qMVW-hMjCR4EZWP8yOhTNrUb4ThPKAa4HsdD7S_z1eGhRrwd3uobIOKnftmYF1TjiHP3WVzlYCA"
+use Wrenchmode::Rack, domain_id: 12, check_delay_secs: 2, logging: true, jwt: "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJEb21haW46MTIiLCJleHAiOjE0NjA2NTY4NzcsImlhdCI6MTQ1ODA2NDg3NywiaXNzIjoiQ3Jhc2hzd2l0Y2giLCJqdGkiOiI3MDIwNTFlOS0wZmNmLTRlZGYtODRlMS0zMWZjMTEzOTg4Y2EiLCJwZW0iOnt9LCJzdWIiOiJEb21haW46MTIiLCJ0eXAiOiJ0b2tlbiJ9.Y90a7cDw_-1qMVW-hMjCR4EZWP8yOhTNrUb4ThPKAa4HsdD7S_z1eGhRrwd3uobIOKnftmYF1TjiHP3WVzlYCA"
 
 get "/" do
   slim :index
